170a6299c6c1ff719320c69334247b3cdffdead6
Taiki130 <air19990130@yahoo.co.jp>
2023-01-15 22:04:31 +0900

    notKinkyuJi

--- 5_package/notKinkyuJi/eaters.go
+++ 5_package/notKinkyuJi/eaters.go
@@ -1,26 +1,19 @@
 package main
 
 import (
+	"os"
 	"fmt"
-	"os" //追加
+	"./shop" // GO111MODULE=off
 )
-
-func Eat(name string) (bool, error) {
-	if name == "" {
-		return false, fmt.Errorf("name is empty.")
-	}
-	fmt.Println(name)
-	return true, nil
-}
 
 func main() {
 	var name1 string = "GYUDON"
-	if _, err := Eat(name1); err != nil {
+	if _, err := shop.Eat(name1); err != nil {
 		fmt.Fprintf(os.Stderr, "cannot eat: '%s'\n" , err) //更新
 	}
 
 	var name2 string = ""
-	if _, err := Eat(name2); err != nil {
+	if _, err := shop.Eat(name2); err != nil {
 		fmt.Fprintf(os.Stderr, "cannot eat: '%s'\n" , err) //更新
 	}
 }
